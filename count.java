ac465a630cd69337ff56908453c474837ea954b7
Prem-pm <premselvam.2906@gmail.com>
2025-03-07 12:40:25 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,33 @@
+package logesstellarday3;
+
+import java.util.Scanner;
+
+public class count {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int n =sc.nextInt();
+		int a[]= new int[n];
+		for(int i=0;i<n;i++){
+		    a[i]=sc.nextInt();
+		}
+		boolean b[]=new boolean[n];
+		 System.out.println();
+		for(int i=0;i<n;i++){
+			if(b[i]==true) {
+				continue;}
+				int count=1;
+				for(int j=i+1;j<n;j++) {
+					if(a[i]==a[j]) {
+						b[j]=true;
+						count++;
+					}
+				}
+				System.out.println(a[i]+" "+count);
+			}
+		 
+		}
+
+	}
+
+
